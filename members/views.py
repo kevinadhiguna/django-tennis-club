441bca56c71ec1d67d4d5985b4fef44f532a82c9
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-01-22 08:51:19 +0700

    refactor: pass members data into the template

--- members/views.py
+++ members/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Member
 
 def members(request):
-  return HttpResponse("Hello World!")
+  members = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'members': members,
+  }
+  return HttpResponse(template.render(context, request))
