5e67da3a4204930473a248f429d1e4e67b2229c7
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-01-22 08:49:17 +0700

    chore: add members endpoint

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'members'
+
 urlpatterns = [
-  path('members/', views.members, name=members)
+  path('members/', views.members, name='members'),
 ]
