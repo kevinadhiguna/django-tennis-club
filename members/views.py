447db085dcc769db8c110e021d7e981d0dfa0acb
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-01-23 12:03:36 +0700

    refactor: add a method to a test page

--- members/views.py
+++ members/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.db.models import Q
 
 from .models import Member
 
@@ -22,3 +23,54 @@ def details(request, id):
 def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
+
+def testing(request):
+  ### -- QuerySet Get -- ###
+
+  members = Member.objects.all().values()
+
+  # Return a specific column
+  firstnames = Member.objects.values_list('firstname')
+  
+  ### ------------------ ###
+
+  ### -- QuerySet Filter (for more: https://www.w3schools.com/django/django_queryset_filter.php) -- ###
+  
+  # Return particular rows
+  emily_data = Member.objects.filter(firstname='Emily').values()
+  refsnes_id2 = Member.objects.filter(lastname='Refsnes', id=2).values()
+  
+  # (1) [extended version]
+  tobias_or_emil = Member.objects.filter(firstname='Emil').values() | Member.objects.filter(firstname='Tobias').values()
+  # (2) [same with (1), but still common] 
+  mesut_or_smith = Member.objects.filter(Q(firstname='Mesut') | Q(firstname='Smith')).values()
+
+  # Field Lookups syntax
+  member_whose_firstname_starts_from_l = Member.objects.filter(firstname__startswith='L').values() # <- ... WHERE firstname LIKE 'L%'
+  ### --------------------- ###
+
+  ### -- QuerySet Order By -- ###
+  members_ordered_by_firstname = Member.objects.all().order_by('firstname').values() # SELECT * FROM members ORDER BY firstname;
+  members_descendingly_ordered_by_firstname = Member.objects.all().order_by('-firstname').values() # SELECT * FROM members ORDER BY firstname DESC;
+
+  # Multiple Order Bys
+  members_multi_ordered_by = Member.objects.all().order_by('lastname', '-id').values() # SELECT * FROM members ORDER BY lastname ASC, id DESC;
+  ### ----------------------- ###
+
+  template = loader.get_template('template.html')
+  context = {
+    'fruits': ['Apple', 'Banana', 'Manggo'],
+    'fav_food': ['Apple', 'Banana', 'Manggo'],
+    'pricey_fruits': ['Grape', 'Tomato', 'Pineapple'],
+    'firstname': 'Linus',
+    'members': members,
+    'greeting': 1,
+    'eat_fruits': True,
+    'day': 'Friday',
+    'firstnames': firstnames,
+    'emily_data': emily_data,
+    'refsnes_id2': refsnes_id2,
+    'tobias_or_emil': tobias_or_emil,
+    'mesut_or_smith': mesut_or_smith,
+  }
+  return HttpResponse(template.render(context, request))
