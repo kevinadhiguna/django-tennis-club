cef9e3191f4d512fdf669d2b42f4a534f823da42
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-01-23 14:38:06 +0700

    fix: fix url of members app starting with a slash

--- members/urls.py
+++ members/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'members'
 
 urlpatterns = [
-  path('/', views.main, name='main'),
+  path('', views.main, name='main'),
   path('members/', views.members, name='members'),
   path('members/detail/<slug:slug>/', views.details, name='details'),
   path('testing/', views.testing, name='testing'),
